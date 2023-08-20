0830fdef3131c64f9a695eb0874c8e49de31e325
Branimir Brekalo <branebrekalo@gmail.com>
2023-08-20 16:25:17 +0200

    Added custom cuda error checking

--- nn-framework/main.cu
+++ nn-framework/main.cu
@@ -1,12 +1,31 @@
+// Standard library
 #include <iostream>
+#include <stdio.h>
 
-__global__ void myKernelTest(void){
+// Nvidia
+#include <driver_types.h>
+#include <cuda_runtime.h>
 
+// Project headers
+#include "nn-framework/utils/error_check_cuda.hpp"
+
+__global__ void myKernelTest(void)
+{
+    int idx = threadIdx.x;
+
+    printf("Thread ID: %d\n", idx);
+
+    return;
 }
 
 int main()
 {
-    myKernelTest<<<1, 1>>>();
-    printf("Radi bkt mazo\n");
+    myKernelTest<<<1, 128>>>();
+    //1024 max
+
+    cuda_check(cudaDeviceSynchronize());
+
+    printf("CUDA radi!\n");
+    
     return 0;
 }
