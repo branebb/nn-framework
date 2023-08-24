745c6ce29cabdd74e81b3996ea6895771d2052d4
Branimir Brekalo <branebrekalo@gmail.com>
2023-08-24 14:37:54 +0200

    Main changed

--- nn-framework/main.cu
+++ nn-framework/main.cu
@@ -32,27 +32,7 @@ int main()
     nn.addLayer(new LinearLayer("linear2", Dimensions(10, 2)));
     nn.addLayer(new SoftmaxActivation("sigmoid"));
 
-    CoordinatesDataset dataset(100, 20);
-
-    Matrix Y;
-    for (int epoch = 0; epoch < 1001; epoch++) 
-    {
-        float cost = 0.0;
-
-        for (int batch = 0; batch < dataset.getNumOfBatches() - 1; batch++) 
-        {
-            Y = nn.forward(dataset.getBatches().at(batch));
-            nn.backprop(Y, dataset.getTargets().at(batch));
-            cost += MSE.cost(Y, dataset.getTargets().at(batch));
-        }
-
-        if (epoch % 100 == 0) 
-        {
-            std::cout << "Epoch: " << epoch
-                << ", Cost: " << cost / dataset.getNumOfBatches()
-                << std::endl;
-        }
-    }
+    
 
     cuda_check(cudaDeviceSynchronize());
 
